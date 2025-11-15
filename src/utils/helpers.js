88927f26b8ee09f989734bf7bd8eebd3e85a2277
KanhaiyaPandey <kanhaiyapandey2232@gmail.com>
2025-11-15 07:47:35 +0530

    enviroment veriables fix

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,16 +1,23 @@
 import axios from "axios";
 
-export const publicFetch = axios.create({
-  baseURL: `${
-    process.env.NEXT_PUBLIC_BASE_URL
-  }/api/aladdin/public`,
-  withCredentials: true, // Ensures cookies are sent
-});
+const isProduction = process.env.NODE_ENV === "production";
 
+const baseURL =
+  process.env.NEXT_PUBLIC_BASE_URL ||
+  (isProduction
+    ? process.env.NEXT_PUBLIC_PRODUCTION_URL
+    : "http://localhost:8080");
+
+export const publicFetch = axios.create({
+  baseURL: `${baseURL}/api/aladdin/public`,
+  withCredentials: true,
+});
 
 export const authFetch = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/user`,
-    withCredentials: true, // Ensures cookies are sent
+  baseURL: `${baseURL}/api/auth/user`,
+  withCredentials: true,
 });
 
-export const gooogleOAuthFetch = `${process.env.DEV_OAUTH_REDIRECT_URI || process.env.PRODUCTION_OAUTH_REDIRECT_URI}`;
+export const gooogleOAuthFetch = isProduction
+  ? process.env.NEXT_PUBLIC_PRODUCTION_OAUTH_REDIRECT_URI
+  : process.env.NEXT_PUBLIC_DEV_OAUTH_REDIRECT_URI;
